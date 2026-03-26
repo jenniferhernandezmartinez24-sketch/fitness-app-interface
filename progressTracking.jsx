8290d8727f5134e415b033d548f95e685ea1055d
jenniferhernandezmartinez24-sketch <jenniferhernandezmartinez24@gmail.com>
2026-03-26 11:43:04 -0600

    Add ProgressTracking component file

---
+++ progressTracking.jsx
@@ -0,0 +1,7 @@
+// src/components/ProgressTracking.jsx
+import React from 'react';
+import { TrendingUp, Award, Zap, Calendar } from 'lucide-react';
+
+export default function ProgressTracking() {
+  // See full code from earlier response
+}
