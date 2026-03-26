2991da4765f0138b38e29fee055e303a8cea933e
jenniferhernandezmartinez24-sketch <jenniferhernandezmartinez24@gmail.com>
2026-03-26 11:33:25 -0600

    Add App.jsx

---
+++ App.jsx
@@ -0,0 +1,13 @@
+// App component
+import React from 'react';
+import Dashboard from './Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
