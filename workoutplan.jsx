16c7e335eac6ba860baef813b00eb8564f51ec9c
jenniferhernandezmartinez24-sketch <jenniferhernandezmartinez24@gmail.com>
2026-03-26 11:57:46 -0600

    Delete workoutplan.jsx

--- workoutplan.jsx
+++
@@ -1,7 +0,0 @@
-// src/components/WorkoutPlan.jsx
-import React, { useState } from 'react';
-import { ChevronLeft, ChevronRight, Dumbbell, Wind, Heart, Zap } from 'lucide-react';
-
-export default function WorkoutPlan() {
-  // See full code from earlier response
-}
