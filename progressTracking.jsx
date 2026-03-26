ee2081566c0fb05a37f5486ece908c77838c991c
jenniferhernandezmartinez24-sketch <jenniferhernandezmartinez24@gmail.com>
2026-03-26 11:57:13 -0600

    Delete progressTracking.jsx

--- progressTracking.jsx
+++
@@ -1,7 +0,0 @@
-// src/components/ProgressTracking.jsx
-import React from 'react';
-import { TrendingUp, Award, Zap, Calendar } from 'lucide-react';
-
-export default function ProgressTracking() {
-  // See full code from earlier response
-}
